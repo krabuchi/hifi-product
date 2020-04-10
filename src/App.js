54ef92a01be27c9193c84822e02ea9d5439e2033
krabuchi <shashu95@yahoo.com>
2020-04-10 12:51:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+let textColor = `#fff`;
+let defaultStyle = {
+  color: textColor
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img/>
+        <input type="text" />
+        <span>Filter</span>
+      </div>
+    );
+  }
+}
+
+class Aggregate extends Component {
+  render(){
+    return (
+      <div className="aggregate" style={{...defaultStyle, width: '40%', display: `inline-block`}}>
+        <h2 style={{color: '#fff'}}>Number Text</h2>
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: '20%', display: `inline-block`}}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li> 
+        </ul>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color: textColor}}>Title</h1>
+      <Aggregate />
+      <Aggregate />
+      <Filter />
+      <Playlist />
+      <Playlist />
+      <Playlist />
     </div>
   );
 }
